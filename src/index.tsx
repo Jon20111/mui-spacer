523dfe380f4992c796d725063b1eccea8e58a126
Jon M <jonathan.middaugh@gmail.com>
2022-07-11 10:45:48 -0500

    remove console

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ interface SpacerProps extends DividerProps {
 
 export default function (props: SpacerProps) {
   const {space, sx, ...rest} = props;
-  console.log(sx);
   const direction = rest?.orientation === 'vertical' ? 'borderRightWidth' : 'borderBottomWidth'
   return (
     <Divider sx={{borderColor: 'transparent', ...sx, ...{[direction]: space ? space : '1px'}}} {...rest}/>
